eaa7f801f21d2739a6424a32bb158fef65fc9179
MosesChuka <chukwukaokonkwo8@gmail.com>
2025-08-17 17:37:01 +0800

    adding with cpu

---
+++ src/add_with_cpu.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <math.h>
+#include <bitset>
+
+// function to add the elements of two arrays
+void add(int n, float *x, float *y)
+{
+ for(int i = 0; i < n; i++)
+    y[i] = x[i] + y[i];
+}
+
+
+int main(void){
+    int N = 1 <<20; // 1 million elements
+    std::cout << "The value N is: "<< N << " Or " << std::bitset<32>(N) << std::endl;
+    std::cout << std::endl;
+
+    float *x = new float[N];
+    float *y = new float[N];
+   
+    // initialize x and y arrays on the host
+    for (int i = 0; i < N; i++){
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    // Run kernel on 1M elements on the CPU
+    add(N,x,y);
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i]-3.0f));
+    std::cout << "Max error: " << std::float_t(maxError) << std::endl;
+
+    delete [] x;
+    delete [] y;
+
+    return 0;
+}
